3c43f5bfbf17a5f21e2cfd74439f76a492697fad
kurabemono <dpmelosantos@gmail.com>
2014-03-19 09:42:30 +0800

    Added GUI messagebox
    BUG: window hangs during recording. Trying to find a workaround using
    threads.

--- recognizer.py
+++ recognizer.py
@@ -1,4 +1,5 @@
 from Tkinter import *
+from threading import Thread
 from record import record_to_file
 from features import mfcc, logfbank
 import scipy.io.wavfile as wav
@@ -7,18 +8,43 @@ class Application(Frame):
 
     def createWidgets(self):
         self.button_image = PhotoImage(file="button.gif")
-        self.RECORD = Button(self, image=self.button_image, width="100", height="100", command=record_and_test)
-        self.RECORD.pack({"side" : "left"})
+        self.RECORD = Button(self, image=self.button_image, width="100", height="100", command=self.record_and_test)
+        self.RECORD.pack()
+        self.TEXTBOX = Text(self, height="1", width="15")
+        self.TEXTBOX.pack()
 
     def __init__(self, master=None):
         Frame.__init__(self, master)
         self.pack()
         self.createWidgets()
+        self.TEXTBOX.insert(INSERT, "Press to record")
+        self.TEXTBOX.tag_config("recording", foreground="red", justify="center")
+        self.TEXTBOX.tag_config("success", foreground="darkgreen", justify="center")
+        self.TEXTBOX.configure(state="disabled")
 
-def record_and_test():
-    record_to_file('demo.wav')
-    (rate,sig) = wav.read('demo.wav')
-    # TODO Feed into ANN
+    def record_and_test(self):
+        self.TEXTBOX.configure(state="normal")
+        self.TEXTBOX.delete("1.0", END)
+        self.TEXTBOX.insert(INSERT, "Recording")
+        self.TEXTBOX.tag_add("recording", "1.0", END)
+        self.TEXTBOX.configure(state="disabled")
+        self.master.update()
+
+        recorder_thread = Thread(target = record_to_file, args=('test.wav',))
+        recorder_thread.start()
+        recorder_thread.join()
+        (rate,sig) = wav.read('test.wav')
+        # TODO Feed into ANN
+
+        self.TEXTBOX.configure(state="normal")
+        self.TEXTBOX.delete("1.0", END)
+        self.TEXTBOX.tag_remove("recording", "1.0")
+        self.TEXTBOX.insert(INSERT, "Completed")
+        self.TEXTBOX.tag_add("success", "1.0", END)
+        self.TEXTBOX.configure(state="disabled")
+
+def record(filename="test.wav"):
+    pass
 
 if __name__ == '__main__':
 
@@ -26,4 +52,4 @@ if __name__ == '__main__':
     root = Tk()
     app = Application(master=root)
     app.mainloop()
-    root.destroy()
+    #root.destroy()
