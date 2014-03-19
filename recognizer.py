821b05e7f791ff75873597fd007164ad3eed8901
kurabemono <dpmelosantos@gmail.com>
2014-03-19 22:05:05 +0800

    Fixed GUI hang
    Moved the recording to a non-blocking thread

--- recognizer.py
+++ recognizer.py
@@ -8,7 +8,7 @@ class Application(Frame):
 
     def createWidgets(self):
         self.button_image = PhotoImage(file="button.gif")
-        self.RECORD = Button(self, image=self.button_image, width="100", height="100", command=self.record_and_test)
+        self.RECORD = Button(self, image=self.button_image, width="100", height="100", command=self.record_buttonpress)
         self.RECORD.pack()
         self.TEXTBOX = Text(self, height="1", width="15")
         self.TEXTBOX.pack()
@@ -22,29 +22,35 @@ class Application(Frame):
         self.TEXTBOX.tag_config("success", foreground="darkgreen", justify="center")
         self.TEXTBOX.configure(state="disabled")
 
-    def record_and_test(self):
-        self.TEXTBOX.configure(state="normal")
-        self.TEXTBOX.delete("1.0", END)
-        self.TEXTBOX.insert(INSERT, "Recording")
-        self.TEXTBOX.tag_add("recording", "1.0", END)
-        self.TEXTBOX.configure(state="disabled")
-        self.master.update()
-
-        recorder_thread = Thread(target = record_to_file, args=('test.wav',))
+    def record_buttonpress(self):
+        recorder_thread = Thread(target=record_and_test, args=(self.TEXTBOX, self.RECORD))
         recorder_thread.start()
-        recorder_thread.join()
-        (rate,sig) = wav.read('test.wav')
-        # TODO Feed into ANN
 
-        self.TEXTBOX.configure(state="normal")
-        self.TEXTBOX.delete("1.0", END)
-        self.TEXTBOX.tag_remove("recording", "1.0")
-        self.TEXTBOX.insert(INSERT, "Completed")
-        self.TEXTBOX.tag_add("success", "1.0", END)
-        self.TEXTBOX.configure(state="disabled")
+def record_and_test(textbox, button, filename="test.wav"):
 
-def record(filename="test.wav"):
-    pass
+    # Disable button and change text
+    button.configure(state="disabled")
+    textbox.configure(state="normal")
+    textbox.delete("1.0", END)
+    textbox.insert(INSERT, "Recording")
+    textbox.tag_add("recording", "1.0", END)
+    textbox.configure(state="disabled")
+
+    # Record to file
+    record_to_file(filename)
+    (rate,sig) = wav.read(filename)
+
+
+    # TODO Feed into ANN
+
+    # Change text and re-enable button
+    textbox.configure(state="normal")
+    textbox.delete("1.0", END)
+    textbox.tag_remove("recording", "1.0")
+    textbox.insert(INSERT, "Completed")
+    textbox.tag_add("success", "1.0", END)
+    textbox.configure(state="disabled")
+    button.configure(state="normal")
 
 if __name__ == '__main__':
 
